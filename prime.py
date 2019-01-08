ad0c8aa7a8275528a86a20a53f37eb5a1d5f4d73
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-01-08 15:46:09 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,15 @@
+#rubi
+number = int(input())
+
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print("no")
+            break
+    else:
+        print("yes")
+
+
+else:
+    print("no")
