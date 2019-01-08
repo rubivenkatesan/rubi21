43702c3f406b9da467e0bf69d44d3679d70fc05b
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-01-08 21:05:09 +0530

    Update odd1.py

--- odd1.py
+++ odd1.py
@@ -1,5 +1,5 @@
 #rubi
 n,k=map(int,input().split(" "))
-l=[i for i in range(n+1,k) if (i%2)!==0]
+l=[i for i in range(n+1,k) if (i%2)!=0]
 k=[print(l[i],end=" ")for i in range(0,len(l)-1)]
 print(l[len(l)-1])
