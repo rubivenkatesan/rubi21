5526b71d0738497137c3af12c22e273bd1bc4232
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-01-08 11:04:52 +0530

    Update large.py

--- large.py
+++ large.py
@@ -1,13 +1,13 @@
 #rubi
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
  
 if (num1 > num2) and (num1 > num3):
-   largest = num1
+   print("num1 is largest")
 elif (num2 > num1) and (num2 > num3):
-   largest = num2
+   print("num2 is largest")
 else:
-   largest = num3
+   print("num3 is largest")
  
-print("The largest number is",largest)
+
