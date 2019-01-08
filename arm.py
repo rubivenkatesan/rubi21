65f4aa1ee39063dbf4fceae520fee030a51ccdfd
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-01-08 16:15:44 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,15 @@
+# rubi
+num = int(input())
+ 
+length = len(str(num))
+sum = 0
+temp = num
+ 
+while(temp != 0):
+	sum = sum + ((temp % 10) ** length)
+	temp = temp // 10
+ 
+if sum == num:
+	print("yes")
+else:
+	print("no")
