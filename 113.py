86350054ca121793aae3224730945c5009acba80
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-01-31 16:16:58 +0530

    Create 113.py

---
+++ 113.py
@@ -0,0 +1,10 @@
+# Python Program to Reverse a Number using While loop    
+     
+Number = int(input())    
+Reverse = 0    
+while(Number > 0):    
+    Reminder = Number %10    
+    Reverse = (Reverse *10) + Reminder    
+    Number = Number //10    
+     
+print(Reverse)   
