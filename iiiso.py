208e12a54e1e5b7c7cf049f1191bd4bc483d8955
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-02-11 15:37:32 +0530

    Update iiiso.py

--- iiiso.py
+++ iiiso.py
@@ -1,13 +1,18 @@
 #rubi
-from collections import Counter
-a,b=map(str,input().split())
-l1=list(a)
-l2=list(b)
-t=Counter(l1)
-x=Counter(l2)
-z=list(t.values())
-y=list(x.values())
-if(z==y):
-    print("yes")
+s1,s2=map(str,input().split())
+c=[]
+d=[]
+for i in s1:
+	c.append(s1.count(i))
+for j in s2:
+	d.append(s2.count(j))
+for i in c:
+	if i in d:
+		flag=1
+	
+	else:
+		flag=0
+if flag==1:
+	print("yes")
 else:
-    print("no")
+	print("no")
