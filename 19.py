33793babcec49101fd6d06e0ea74d44d1dec7ee5
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-02-12 16:10:42 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,20 @@
+#rubi
+n=int(input())
+l=[]
+flag=1
+a=[]
+s=""
+for i in range(2,n+1):
+    for j in range(2,n+1):
+        if i*j==n:
+            l.append(i)
+for i in l:
+    for j in range(2,i):
+        if i%j==0:
+            flag=0
+            break
+        else:
+            flag=1
+    if flag==1:
+        s=s+str(i)+" "
+print(s.strip())
