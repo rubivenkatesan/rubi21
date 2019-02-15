9957fc8bae81d9428e9c45f2b691cb05525f1534
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-02-15 15:16:22 +0530

    Create guvi23.py

---
+++ guvi23.py
@@ -0,0 +1,9 @@
+#rubi
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+s=list(map(int,input().split()))
+k=""
+for i in s:
+    l.append(i)
+    k=k+str(max(l))+" "
+print(k.strip())
