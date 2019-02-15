e59107233d15780ceb0cc8dbd668902c78b9db1f
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-02-15 15:39:49 +0530

    Update guvi35.py

--- guvi35.py
+++ guvi35.py
@@ -1,7 +1,11 @@
 #rubi
 s=input()
 k=""
+l=[]
 for i in s:
-    if s.count(i)==1 and i!=" ":
-        k=k+i+" "
+	l.append(s.count(i))
+min1=min(l)
+for i in s:
+	if s.count(i)==min1 and i!=" ":
+		k=k+i.lower()+" "
 print(k.strip())
