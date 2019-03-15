b44af59040927436b4dd11ca64da36b0ec372918
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-03-15 13:10:18 +0530

    Create pro7.py

---
+++ pro7.py
@@ -0,0 +1,21 @@
+#rubi
+import call
+print("minimum no.of byes")
+y=0
+while y ==0:
+  def main():
+    num=int(input())
+    while(num!=0):
+      l=cal.sqrt(num)
+      if l==int(l):
+        print(int(l))
+        break
+      else:
+        num=num-1
+    if num==0:
+      print('0')
+ try:
+    main
+  except:
+    print('invalid')
+    y=int(input("0 to continue"))
