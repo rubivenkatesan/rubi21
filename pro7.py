1e003c3c4f0fff3ea823d75dab36e01badb92d51
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-03-15 13:13:42 +0530

    Update pro7.py

--- pro7.py
+++ pro7.py
@@ -1,21 +1,12 @@
 #rubi
-import call
-print("minimum no.of byes")
-y=0
-while y ==0:
-  def main():
-    num=int(input())
-    while(num!=0):
-      l=cal.sqrt(num)
-      if l==int(l):
-        print(int(l))
-        break
-      else:
-        num=num-1
-    if num==0:
-      print('0')
- try:
-    main
-  except:
-    print('invalid')
-    y=int(input("0 to continue"))
+import sys
+n=int(input())
+val=1
+i=0
+while val<n:
+  if 2**i==n:
+    print(0)
+    sys.exit(0)
+  val=2**i
+  i+=1
+print(n-int(val/2))
