b12fb2defef2a003219ca06fbe0bcf8ab12f1487
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-03-16 12:55:16 +0530

    Create pro23.py

---
+++ pro23.py
@@ -0,0 +1,28 @@
+#rubi
+s=input()
+p1,p2=0,0
+d=1
+for i in s:
+  if i=='G':
+    if d==1:
+      p1+=1
+    elif d==2:
+      p2-=1
+    elif d==3:
+      p1-=1
+    elif d==4:
+      p2+=1
+  elif i=='L':
+    if d==4:
+      d=1
+    else:
+      d+=1
+  elif i=='R':
+    if d==1:
+      d=4
+    else:
+      d-=1
+if p1==0 and p2==0:
+  print('yes')
+else:
+  print('no')s
