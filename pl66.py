b0f7a8aea5b87171f81b54e1cf74f879ade02962
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-05-14 10:27:48 +0530

    Create pl66.py

---
+++ pl66.py
@@ -0,0 +1,9 @@
+#rubi
+import collections
+n = list(map(int,input().split()))
+mem = list(map(str,input().split()))
+dic = dict(collections.Counter(mem))
+k = n[len(n)-1]
+for x,y in dic.items():
+    if(y == k):
+        print(x)
