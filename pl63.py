559281f73a599380e6c9124af9d84326d6918623
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-05-14 10:00:03 +0530

    Create pl63.py

---
+++ pl63.py
@@ -0,0 +1,13 @@
+#rubi
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+j=0
+s=""
+for i in range(0,len(a)):
+    for j in range(0,len(b)):
+        if a[i]==b[j]:
+            s=s+str(a[i])+" "
+            b.remove(b[j])
+            break
+print(s.strip())
