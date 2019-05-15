e96c493c558328d2a6ce96e6de6bbee7ffd66aa0
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-05-15 09:51:09 +0530

    Update pl94.py

--- pl94.py
+++ pl94.py
@@ -1,10 +1,11 @@
 #rubi
-n=int(input())
+n=input()
 c=0
-for i in range(n-1):
-  c+=1
-  if(c>1):
+for i in range(0,len(n)):
+    for j in range(i+1,len(n)):
+        if n[i]==n[j]:
+            c=c+1
+if c>=1:
     print("yes")
-    break
 else:
-  print("no")    
+    print("no")
