998968110b025e1b50b33fc4803ae0216d2114d5
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-05-23 18:33:04 +0530

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,14 @@
+#rubi
+r=int(input())
+a=[]
+k=bin(2**r-1)[2::]
+l=len(k)
+for i in range(0,2**r):
+	s=bin(i)[2::]
+	if len(s)<l:
+		a.append([s.count("1"),(l-len(s))*"0"+s])
+	else:
+		a.append([s.count("1"),s])
+a.sort()
+for i in range(0,len(a)):
+	print(a[i][1])
