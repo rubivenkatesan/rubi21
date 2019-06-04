f92e6451539010aea59274f17050ed7a48c276e2
rubivenkatesan <46440488+rubivenkatesan@users.noreply.github.com>
2019-06-04 18:04:47 +0530

    Create hun56.py

---
+++ hun56.py
@@ -0,0 +1,10 @@
+#r
+n=int(input())
+l=[int(x) for x in input().split()]
+a=1000
+for i in range(0,len(l)-1):
+    for j in range(i+1,len(l)):
+        if abs(l[i]+l[j])<a:
+            a=abs(l[i]+l[j])
+            r,g=l[i],l[j]
+print(r,g)
